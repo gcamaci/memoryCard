18cda02e5bafe01c89ee586e07623642ac3c469e
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-05-02 13:09:49 -0500

    fixing push

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ name, imageUrl, playRound }) => {
    return (
     <div className="card w-1/4 shadow-xl" onClick={playRound}>
         <img src={imageUrl} alt={name}></img>
-        <p>{name}</p>gir
+        <p>{name}</p>
     </div>
    )
 }
